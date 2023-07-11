9831e6d12e03e3e61af24d619fb137a800cdb4d0
Justus Karlsson <karlssonjustus@gmail.com>
2023-07-11 17:55:04 +0200

    Basic chat window

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
+    import type { ChatCompletionRequestMessage } from "openai";
   import TextInput from "./lib/elements/TextInput.svelte";
   import View from "./lib/elements/View.svelte";
+
+
+  let messages: ChatCompletionRequestMessage[] = [];
+  let test = true;
+  if (test) {
+    messages = [{content: "Hello!", role: "user"}, {content: "Why hello there!", role: "assistant"},]
+  }
 
 </script>
 
 <main>
   <View class="flex flex-col h-screen items-center bg-secondary" >
-    <View class="h-[100%] w-md ">
-      bla
+    <View class="h-[100%] w-md overflow-y-auto overflow-x-hidden mt-2
+                 flex flex-col items-center space-y-2">
+      {#each messages as message}
+        <View class="p-2 bg-primary">
+          {message.content}
+        </View>
+      {/each}
     </View>
-    <TextInput class="py-4 w-md" 
-
-    />
+    <TextInput class="py-4 w-md" />
   </View>
 </main>
 
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,48 @@
+import type { ChatCompletionRequestMessage, CreateChatCompletionRequest } from "openai"
+
+const PROD = false;
+
+export function apiFetch(
+  path: string,
+  init?: RequestInit
+):
+ Promise<Response> {
+  const BASE = PROD ? "/api" : "http://localhost:3000/api"; 
+  return fetch(BASE + path, init);
+}
+
+
 export function stylesString(styles: Record<string, any>){
   let res = "";
   for (let [key, val] of Object.entries(styles)) {
     res += `${key}: ${val}`;
+  }
+}
+
+
+export async function* MessageStreamGen(body: CreateChatCompletionRequest, endpoint: string = "/chat"){
+  const response = await apiFetch(endpoint, {
+    method: "POST",
+    headers: {
+    'Content-Type': 'text/event-stream'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const reader = response.body!.pipeThrough(new TextDecoderStream()).getReader();
+  while (true) {
+    const {value, done} = await reader.read();
+    if (done) break;
+    const chunks = value.split("data: ");
+    for (let i = 1; i < chunks.length; i++) {
+      if (chunks[i].startsWith("[DONE]")) {
+        break;
+      }
+      const data = JSON.parse(chunks[i]);
+      const delta = data.choices[0].delta;
+      if ("content" in delta) {
+        yield delta.content as string;
+      }
+    }
   }
 }
