4e30b186fa029661f2dc64153a8ddaf8818deb06
Justus Karlsson <karlssonjustus@gmail.com>
2023-07-13 11:49:10 +0200

    Add hacky router

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,27 @@
 <script lang="ts">
-    import type { ChatCompletionRequestMessage } from "openai";
-  import TextInput from "./lib/elements/TextInput.svelte";
-  import View from "./lib/elements/View.svelte";
-    import { MessageStreamGen, SYS_CLASSIFY_1, SYS_TROUBLESHOOT_1 } from "./lib/util";
-
-
-  let messages: ChatCompletionRequestMessage[] = [];
-  let test = false;
-  if (test) {
-    messages = [{content: "Hello!", role: "user"}, {content: "Why hello there!", role: "assistant"},]
-  }
-
-  async function sendMessage(input: string){
-    let oldMessages = [...messages];
-    let prompt: ChatCompletionRequestMessage = {
-      content: input,
-      role: "user",
-    }
-    const stream = MessageStreamGen({
-      messages: [
-        SYS_TROUBLESHOOT_1,
-        ...oldMessages,
-        prompt,
-      ],
-      model: "gpt-3.5-turbo"
-    })
-    let answer: ChatCompletionRequestMessage = {
-      content: "",
-      role: "assistant",
-    }
-    messages = [...oldMessages, prompt, answer];
-    for await ( let chunk of stream) {
-      answer.content += chunk;
-      messages = [...oldMessages, prompt, answer];
-    }
-  }
-
+  import type { SvelteComponent } from 'svelte';
+  import {Route as OrgRoute} from 'tinro'; 
+    import ChatView from './lib/components/ChatView.svelte';
+  const Route2 = OrgRoute as unknown;
+  const Route = Route2 as typeof SvelteComponent;
 </script>
 
-<main>
-  <View class="flex flex-col h-screen items-center bg-secondary" >
-    <View class="h-[100%] w-md overflow-y-auto overflow-x-hidden mt-2
-                 flex flex-col items-center space-y-2">
-      {#each messages as message}
-        <View class="p-2 bg-primary">
-          {message.content}
-        </View>
-      {/each}
-    </View>
-    <TextInput class="py-4 w-md" onSubmit={sendMessage} placeholder="Describe your problem.." />
-  </View>
-</main>
+<nav>
+  <a href="/">Home</a>
+  <a href="/portfolio">Portfolio</a>
+  <a href="/contacts">Contacts</a>
+</nav>
 
+<Route path="/"><ChatView /></Route>
+<Route path="/portfolio/*">
+  <Route path="/">
+      <h1>Portfolio introduction</h1>
+      <nav>
+          <a href="/portfolio/sites">Sites</a> 
+          <a href="/portfolio/photos">Photos</a>
+      </nav>
+  </Route>
+  <Route path="/sites"><h1>Portfolio: Sites</h1></Route>
+  <Route path="/photos"><h1>Portfolio: Photos</h1></Route>
+</Route>
 
---
+++ src/lib/components/ChatView.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import type { ChatCompletionRequestMessage } from "openai";
+import TextInput from "../elements/TextInput.svelte";
+import View from "../elements/View.svelte";
+  import { MessageStreamGen, SYS_CLASSIFY_1, SYS_TROUBLESHOOT_1 } from "../util";
+
+
+let messages: ChatCompletionRequestMessage[] = [];
+let test = false;
+if (test) {
+  messages = [{content: "Hello!", role: "user"}, {content: "Why hello there!", role: "assistant"},]
+}
+
+async function sendMessage(input: string){
+  let oldMessages = [...messages];
+  let prompt: ChatCompletionRequestMessage = {
+    content: input,
+    role: "user",
+  }
+  const stream = MessageStreamGen({
+    messages: [
+      SYS_TROUBLESHOOT_1,
+      ...oldMessages,
+      prompt,
+    ],
+    model: "gpt-3.5-turbo"
+  })
+  let answer: ChatCompletionRequestMessage = {
+    content: "",
+    role: "assistant",
+  }
+  messages = [...oldMessages, prompt, answer];
+  for await ( let chunk of stream) {
+    answer.content += chunk;
+    messages = [...oldMessages, prompt, answer];
+  }
+}
+
+</script>
+
+<main>
+<View class="flex flex-col h-screen items-center bg-secondary" >
+  <View class="h-[100%] w-md overflow-y-auto overflow-x-hidden mt-2
+               flex flex-col items-center space-y-2">
+    {#each messages as message}
+      <View class="p-2 bg-primary">
+        {message.content}
+      </View>
+    {/each}
+  </View>
+  <TextInput class="py-4 w-md" onSubmit={sendMessage} placeholder="Describe your problem.." />
+</View>
+</main>
